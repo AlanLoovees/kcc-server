f02d0bdc9caf1a06bc3cf6a39b7bb4f4d902bfb4
Alan Loovees <alanloovees@gmail.com>
2021-02-28 12:20:46 +0530

    local storage and refresh after 6pm IST implemented

--- app.js
+++ app.js
@@ -9,16 +9,55 @@ const app = express();
 app.use(cors())
 const server = http.createServer(app);
 
+const refresh = 18*60
+const date = new Date()
+const now = date.getHours()*60 + date.getMinutes()
+
+function checkTime() {
+  if (refresh <= now) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
 app.get("/fetchStateWise", (req, res) => {
-  fetch("https://api.covid19india.org/data.json")
+  if (checkTime()) {
+    fetch("https://api.covid19india.org/data.json")
     .then((response) => response.json())
-    .then((data) => {res.send(data.statewise[5])});
+    .then((data) => {
+      let write = JSON.stringify(data.statewise[5])
+      fs.writeFile('stateWise.json', write, (err) => {if(err) throw err})
+      res.send(data.statewise[5])
+    });
+  }
+  else {
+    fs.readFile('stateWise.json', (err, data) => {
+      if (err) throw err
+      let read = JSON.parse(data)
+      res.send(read)
+    });
+  }
 });
 
 app.get("/fetchDistrictWise", (req, res) => {
-  fetch("https://api.covid19india.org/state_district_wise.json")
+  if (checkTime()) {
+    fetch("https://api.covid19india.org/state_district_wise.json")
     .then((response) => response.json())
-    .then((data) => {res.send(data.Kerala.districtData)});
+    .then((data) => {
+      let write = JSON.stringify(data.Kerala.districtData)
+      fs.writeFile('districtWise.json', write, (err) => {if(err) throw err})
+      res.send(data.Kerala.districtData)
+    });
+  }
+  else {
+    fs.readFile('districtWise.json', (err, data) => {
+      if (err) throw err
+      let read = JSON.parse(data)
+      res.send(read)
+    });
+  }
 });
 
 const port = process.env.PORT || 3000;
